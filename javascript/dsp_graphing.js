6ccd0943f428a20dfe54ae4d14cf737d555a6ade
Sam Koblenski <koblenski@gmail.com>
2015-10-22 22:19:12 -0500

    Add function to draw a dual axis

--- javascript/dsp_graphing.js
+++ javascript/dsp_graphing.js
@@ -94,6 +94,62 @@ function drawZeroAxis(stage) {
   stage.addChild(xLabel);
 }
 
+function drawDualAxis(stage) {
+  var graphics = new PIXI.Graphics();
+  graphics.lineStyle(1, 0xdddddd, 1);
+  graphics.moveTo(15, 85);
+  graphics.lineTo(535, 85);
+
+  for(var x = 41; x <= 535; x += 26) {
+    graphics.moveTo(x, 80);
+    graphics.lineTo(x, 85);
+  }
+  graphics.moveTo(275, 75);
+  graphics.lineTo(275, 85);
+
+  graphics.moveTo(15, 20);
+  graphics.lineTo(15, 150);
+  for(var y = 33; y <= 150; y += 26) {
+    graphics.moveTo(15, y);
+    graphics.lineTo(20, y);
+  }
+
+  graphics.moveTo(15, 228);
+  graphics.lineTo(535, 228);
+
+  for(var x = 41; x <= 535; x += 26) {
+    graphics.moveTo(x, 223);
+    graphics.lineTo(x, 228);
+  }
+  graphics.moveTo(275, 218);
+  graphics.lineTo(275, 228);
+
+  graphics.moveTo(15, 176);
+  graphics.lineTo(15, 280);
+  for(var y = 176; y <= 280; y += 26) {
+    graphics.moveTo(15, y);
+    graphics.lineTo(20, y);
+  }
+
+  stage.addChild(graphics);
+
+  var style = { font: 'italic 14px Arial', fill: '#eeeeee' };
+  var x_text = new PIXI.Text('t', style);
+  x_text.x = 540;
+  x_text.y = 77;
+  stage.addChild(x_text);
+
+  var y_text = new PIXI.Text('f(t)', style);
+  y_text.x = 10;
+  y_text.y = 2;
+  stage.addChild(y_text);
+
+  var xLabel = new PIXI.Text('1', { font: '14px Arial', fill: '#eeeeee' });
+  xLabel.x = 273;
+  xLabel.y = 87;
+  stage.addChild(xLabel);
+}
+
 function drawSine(sinewave, amplitude, offset, freq, phase, start, end) {
   start = typeof start != 'undefined' ? start : 0
   end = typeof end != 'undefined' ? end : 520
@@ -108,3 +164,4 @@ function drawSine(sinewave, amplitude, offset, freq, phase, start, end) {
   y = amplitude*Math.sin((end*freq + phase)/260.0*Math.PI) + offset;
   sinewave.lineTo(end, -y);
 }
+
